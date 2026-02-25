460371600d699c05bccfbdaf908d49c8722c411e
mendezgael <mendez.mateo.ju@gmail.com>
2026-02-25 09:23:18 -0300

    feat(facturas): exponer rutas CRUD en FacturasController (PUT, DELETE lógico)

--- src/facturas/facturas.controller.ts
+++ src/facturas/facturas.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { FacturasService } from './facturas.service';
 
 @Controller('facturas')
@@ -18,5 +18,15 @@ export class FacturasController {
   @Post()
   create(@Body() dto: any) {
     return this.facturasService.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: any) {
+    return this.facturasService.update(+id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.facturasService.remove(+id);
   }
 }
