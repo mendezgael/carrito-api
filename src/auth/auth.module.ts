4f3ebfa1a292bbbe58c39e4f14944f0290b10787
mendezgael <mendez.mateo.ju@gmail.com>
2026-02-26 16:25:32 -0300

    feat(auth): Se suma JwtStrategy a providers en módulo

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,19 @@ import { AuthController } from './auth.controller';
 import { UsuariosModule } from 'src/usuarios/usuarios.module';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
   imports: [
     UsuariosModule,
+    PassportModule,
     JwtModule.register({
       secret: 'secreto_super_seguro',
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
