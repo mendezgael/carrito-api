14c1bf54e57d44f83e1f41fedaee858ccf7971d7
mendezgael <mendez.mateo.ju@gmail.com>
2026-02-26 11:08:19 -0300

    feat(auth): se aplico JwtModule en el modulo

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UsuariosModule } from 'src/usuarios/usuarios.module';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  imports: [UsuariosModule],
+  imports: [
+    UsuariosModule,
+    JwtModule.register({
+      secret: 'secreto_super_seguro',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
