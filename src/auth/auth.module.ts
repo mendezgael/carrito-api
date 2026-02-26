539df0669c795bddc3218fd0a11d21f5fbc65c8c
mendezgael <mendez.mateo.ju@gmail.com>
2026-02-26 10:47:02 -0300

    feat: se anadieron los imports y providers a auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { UsuariosModule } from 'src/usuarios/usuarios.module';
+import { AuthService } from './auth.service';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [UsuariosModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
